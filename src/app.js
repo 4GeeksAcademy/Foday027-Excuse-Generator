d425dfed6bf204ffcc8b098e46c769cbc3668962
Foday027 <159056257+Foday027@users.noreply.github.com>
2024-03-17 20:42:54 +0000

    excuse generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,36 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  function rand(length) {
+    let number = Math.floor(Math.random() * length);
+    return number;
+  }
+  let sentence =
+    who[rand(who.length)] +
+    " " +
+    action[rand(action.length)] +
+    " " +
+    what[rand(what.length)] +
+    " " +
+    when[rand(when.length)];
+
+  const el = document.getElementById("excuse");
+  el.innerHTML = sentence;
+  el.style.color = "red";
+  el.style.backgroundColor = "grey";
+  el.style.height = "50px";
+  el.style.fontSize = "30px";
 };
+window.onload();
